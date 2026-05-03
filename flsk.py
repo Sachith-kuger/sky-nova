099270706edf520deffee24e561e979a1a4ea07a
Sachith k <kodaguguruswamy@gmail.com>
2026-05-03 21:51:44 +0530

    Fixed MYSQL_HOST env bug

--- flsk.py
+++ flsk.py
@@ -1,3 +1,5 @@
+import pymysql
+pymysql.install_as_MySQLdb()
 from flask import Flask, jsonify, render_template, request, session, redirect, url_for, flash
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -8,11 +10,12 @@ app = Flask(__name__)
 app.secret_key = 'aero_secret_key_123'
 
 import os
-app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', '127.0.0.1')
-app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'root')
-app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'root123')
-app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'airline_system')
-app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT', 3307))
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
 
 mysql = MySQL(app)
 
